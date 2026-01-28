13460dcda93447f445c4368a7af275a827d43655
Priyatech25 <kpriyadharshinircr@gmail.com>
2026-01-28 10:26:57 +0530

    Day 12: calculator using functions

---
+++ functions/calculator.py
@@ -0,0 +1,7 @@
+def add(a, b): return a + b
+def sub(a, b): return a - b
+def mul(a, b): return a * b
+def div(a, b): return a / b if b != 0 else "Error"
+
+a, b = map(int, input().split())
+print(add(a,b), sub(a,b), mul(a,b), div(a,b))
