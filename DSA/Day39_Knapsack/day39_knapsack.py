3b82f4b2f4bfe63df2ff355e1575e3f69d6f1273
Priyatech25 <kpriyadharshinircr@gmail.com>
2026-04-14 12:42:06 +0530

    Day 39 - 0/1 Knapsack DP Problem

---
+++ DSA/Day39_Knapsack/day39_knapsack.py
@@ -0,0 +1,29 @@
+# DAY 39 - Dynamic Programming (0/1 Knapsack)
+
+def knapsack(weights, values, capacity):
+    n = len(values)
+
+    # DP table
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(1, capacity + 1):
+
+            # item can be included
+            if weights[i - 1] <= w:
+                dp[i][w] = max(
+                    values[i - 1] + dp[i - 1][w - weights[i - 1]],
+                    dp[i - 1][w]
+                )
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    return dp[n][capacity]
+
+
+# Testing
+weights = [1, 3, 4, 5]
+values = [1, 4, 5, 7]
+capacity = 7
+
+print("Maximum Value:", knapsack(weights, values, capacity))
