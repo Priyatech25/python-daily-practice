471ce9fe043ee3f477f9462fde0d44fc07fcee7f
Priyatech25 <kpriyadharshinircr@gmail.com>
2026-06-29 14:43:21 +0530

    quick sort

---
+++ 100DaysOfCode/Day21/quick_sort.py
@@ -0,0 +1,25 @@
+# Day 22 - Quick Sort
+
+def quick_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+
+print("Original Array:")
+print(numbers)
+
+sorted_array = quick_sort(numbers)
+
+print("\nSorted Array:")
+print(sorted_array)
